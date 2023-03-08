c2793b367fc88b3585f81ef8a3d4725f32eba641
Santiago <santidasilva1408@gmail.com>
2023-03-08 15:01:42 -0300

    get routes donne

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -29,26 +29,39 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn
+
+  const filteredBooks = Object.values(books).filter(book => book.isbn === isbn);
+
+  return res.send(filteredBooks);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author
+
+  const filteredBooks = Object.values(books).filter(book => book.author === author);
+
+  return res.send(filteredBooks);
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title
+
+  const filteredBooks = Object.values(books).filter(book => book.title === title);
+
+  return res.send(filteredBooks);
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+
+  const filteredBooks = Object.values(books).filter(book => book.isbn === isbn);
+  const reviews = filteredBooks.map(book => book.reviews);
+
+  return res.send(reviews);
 });
 
 module.exports.general = public_users;
